ce2e484266479faf5fd0f25b5c5c6ed13fb4ec5e
KiHwan Yun <drg2524@naver.com>
2024-01-08 15:49:13 +0900

    spring interceptor log check commit

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -18,6 +18,21 @@ import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /*
+    *  [ FilterRegistrationBean ]
+    *
+    *   필터를 등록하는 방법은 여러가지가 있지만, spring boot를 사용한다면 FilterRegistrationBean을 사용해서 등록하면 된다.
+    *
+    *   setFilter(new LogFilter()) -> 등록할 필터를 지정한다.
+    *   setOrder(1) -> 필터는 체인으로 동작한다. 따라서 순서가 필요하다. 낮을 수록 먼저 동작한다.
+    *   addUrlPatterns("/*") -> 필터를 적용할 URL 패턴을 지정한다. 한번에 여러 패턴을 지정할 수 있다.
+    *
+    *
+    *   [참고]
+    *
+    * @ServletComponentScan @WebFilter(filterName = "logFilter" , urlPatterns = "/*")로 필터 등록이 가능하지만
+    * 필터 순서 조절이 안된다. 따라서 FilterRegistrationBean을 사용하자.
+    * */
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
